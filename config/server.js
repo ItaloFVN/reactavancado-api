a13b61dfef05d375a5df297d50d67864a9af9337
Italo Nascimento <italo.nascimento@dtidigital.com.br>
2022-03-17 23:35:50 -0300

    mudança para corrigir o erro no heroku

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
-module.exports = ({ env }) => ({
+mmodule.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4dc55a73-549c-4278-8ee8-f0ed484fbbb9'),
+    },
   },
 });
