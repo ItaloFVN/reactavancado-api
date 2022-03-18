f3fa07f3879aa8c456f211563f287fbca957fbe4
Italo Nascimento <italo.nascimento@dtidigital.com.br>
2022-03-17 23:21:35 -0300

    Mudança para correção erro 503

--- config/database.js
+++ config/database.js
@@ -1,18 +1,22 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'PostgrePass'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)
-      }
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'PostgrePass'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        },
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
-    options: {
-      ssl: env.bool('DATABASE_SSL', false),
-    }
   },
 });
