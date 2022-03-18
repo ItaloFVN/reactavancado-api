51480f832abaf8e17cf5563fc910af258aef8cde
Italo Nascimento <italo.nascimento@dtidigital.com.br>
2022-03-17 23:45:29 -0300

    Configurações para rodar o heroku

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
-mmodule.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '4dc55a73-549c-4278-8ee8-f0ed484fbbb9'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
