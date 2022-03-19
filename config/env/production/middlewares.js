05c38a82742298135701d3bf7cbcbc717bb1bf00
Italo Nascimento <italo.nascimento@dtidigital.com.br>
2022-03-18 23:18:39 -0300

    Ajuste security middleware

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,4 +1,13 @@
 module.exports = [
+    'strapi::errors',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
     {
         name: 'strapi::security',
         config: {
